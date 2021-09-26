3582959949e7be8fb9593be89669859702c523e0
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-09-26 18:27:13 +0200

    navbar design :rocket:

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {CollectionComponent} from "./components/collection/collection.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
+import {HighlightItemComponent} from "./components/highlight-item/highlight-item.component";
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'collection', component: CollectionComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'highlight', component: HighlightItemComponent},
   {path: '', component: HomeComponent}
 ]
 @NgModule({
