237ce37ddf945fc2ea0945d7556e78165e10bb7d
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-10-18 12:03:13 +0200

    regisration component design :rocket:

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {LoginComponent} from "./components/login/login.component";
 import {CollectionComponent} from "./components/collection/collection.component";
 import {CheckoutComponent} from "./components/checkout/checkout.component";
 import {HighlightItemComponent} from "./components/highlight-item/highlight-item.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegisterComponent},
   {path: 'collection', component: CollectionComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'highlight', component: HighlightItemComponent},
